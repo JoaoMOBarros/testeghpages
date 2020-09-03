649e9769fab620055c7856a664d12e43417a36da
Joao Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-09-03 15:51:36 -0300

    Updates

---
+++ precache-manifest.7c401fc5f9d08b66643d1268d2ef8808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a9ea6ad43690dfab55edcab7607118",
+    "url": "/testeghpages/index.html"
+  },
+  {
+    "revision": "b93f94c0589fcc7aad7a",
+    "url": "/testeghpages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b290809fd9b5dfb13d7a",
+    "url": "/testeghpages/static/js/2.83861a74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testeghpages/static/js/2.83861a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93f94c0589fcc7aad7a",
+    "url": "/testeghpages/static/js/main.a979c541.chunk.js"
+  },
+  {
+    "revision": "0648948f6ef5aefba4e3",
+    "url": "/testeghpages/static/js/runtime-main.3ac03365.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testeghpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
